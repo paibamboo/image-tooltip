a3ada02872bf85a41a93918a7d92d9ec4420a164
thezzz <gavinsitti@hotmail.com>
2016-06-11 10:57:59 +0700

    Improve image position / Change options to background color and image width

--- image-tooltip.js
+++ image-tooltip.js
@@ -2,19 +2,25 @@
     $.fn.imageTooltip = function (options) {
 
         var defaults = {
+            imgWidth: 'initial',
+            backgroundColor: '#fff'
         };
 
         if (typeof (options) === 'object') {
             options = $.extend(defaults, options);
         } else {
             var tempOptions = {};
+            tempOptions.imgWidth = arguments[0] || defaults.imgWidth;
+            tempOptions.backgroundColor = arguments[1] || defaults.backgroundColor;
             options = tempOptions;
         }
 
         function calLeft(x, imgWidth) {
+            return window.innerWidth - x > imgWidth ? x : x - imgWidth;
         }
 
         function calTop(y, imgHeight) {
+            return window.innerHeight - y > imgHeight ? y : y - imgHeight - 25;
         }
 
         return this.each(function () {
@@ -39,6 +45,8 @@
             $(this).hover(
                 function (e) {
                     imgContainer.css({
+                        left: calLeft(e.clientX, imgContainer.outerWidth()) + 'px',
+                        top: calTop(e.clientY, imgContainer.outerHeight()) + 'px'
                     });
                     $('body').append(imgContainer);
                     imgContainer.fadeIn('fast');
@@ -48,6 +56,8 @@
                 }
             ).mousemove(function (e) {
                 imgContainer.css({
+                    left: calLeft(e.clientX, imgContainer.outerWidth()) + 'px',
+                    top: calTop(e.clientY, imgContainer.outerHeight()) + 'px'
                 });
             });
         });
